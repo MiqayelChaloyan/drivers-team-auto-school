229b4eed712f6d6e973bc5ad279694c81d95722e
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-06-21 01:18:10 +0400

    configure project

---
+++ src/constants/config.ts
@@ -0,0 +1,15 @@
+export const site = {
+    title: 'Drivers Team Auto School',
+    description: 'An open source e-commerce project built by inifarhan',
+    url: process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000',
+    authorName: 'driversteam',
+    authorWebsite: 'driversteam',
+    authorUsername: '@driversteam',
+    category: 'autoschool',
+    locale: 'am',
+    keywords: [
+        'autoschool',
+        'Auto School',
+        'Drivers',
+    ]
+};
---
+++ src/utils/default-metadata.ts
@@ -0,0 +1,28 @@
+import { site } from '@/constants/config';
+
+export const defaultMetadata = {
+    metadataBase: new URL(site.url),
+    title: site.title,
+    description: site.description,
+    category: site.category,
+    robots: { index: true },
+    authors: { name: site.authorName },
+    keywords: site.keywords,
+    creator: site.authorUsername,
+    publisher: site.authorUsername,
+    openGraph: {
+        title: site.title,
+        description: site.description,
+        url: site.url,
+        siteName: site.title,
+        images: 'https://skaters-inifarhan.vercel.app/images/screenshoot.PNG',
+        type: 'website',
+        locale: site.locale
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title:  site.title,
+        description: site.description,
+        images: ['https://skaters-inifarhan.vercel.app/images/screenshoot.PNG'],
+    },
+};
