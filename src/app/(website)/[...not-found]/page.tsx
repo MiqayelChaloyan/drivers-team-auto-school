3cf970df0a66fc458a3a8c3d6e11efcbe932f7fc
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-06-23 20:30:03 +0400

    fix

--- src/app/(website)/[...not-found]/page.tsx
+++ src/app/(website)/[...not-found]/page.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
             <body>
                 <div>
                     <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-                        <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+                        {/* <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
                             <div className="relative">
                                 <div className="absolute">
                                     <div className>
@@ -23,7 +23,7 @@ export default function NotFound() {
                                     <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div>
                             <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
                         </div>
