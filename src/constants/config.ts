38876a9eab5b080cdcc459f282ab4a38572fd504
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-08-26 16:19:18 +0400

    fix

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const site = {
-    title: 'Drivers Team Auto School',
-    description: 'Drivers Team Auto School ավտոդպրոցը Հայաստանում առաջատարներից է՝ մասնագիտացված ավտովարման որակյալ և բարձրակարգ ուսուցում տրամադրելու ոլորտում:',
+    title: 'Drivers՚ Team Auto School',
+    description: 'Drivers՚ Team Auto School ավտոդպրոցը Հայաստանում առաջատարներից է՝ մասնագիտացված ավտովարման որակյալ և բարձրակարգ ուսուցում տրամադրելու ոլորտում:',
     url: process.env.NEXT_PUBLIC_DOMAIN ? process.env.NEXT_PUBLIC_DOMAIN : `http://localhost:${process.env.PORT || 3000}`,
-    authorWebsite: 'driversteam',
-    authorUsername: '@driversteam',
+    authorWebsite: 'drivers՚team',
+    authorUsername: '@drivers՚team',
     category: 'autoschool',
-    creator: '@driversteam',
+    creator: '@drivers՚team',
     locale: 'am',
     keywords: [
         'autoschool',
